156d1026963859fc1642def08a5834048169d9a9
perraid <davidperrai@gmail.com>
2015-11-19 17:59:21 +0100

    adding the add cart function

--- servicesshop/src/main/java/fr/alma/servicesshop/ServiceShop.java
+++ servicesshop/src/main/java/fr/alma/servicesshop/ServiceShop.java
@@ -3,6 +3,9 @@ package fr.alma.servicesshop;
 import org.apache.axis2.AxisFault;
 
 
+import fr.alma.servicesshop.paymentservice.PaymentServiceStub;
+import fr.alma.servicesshop.shoppingcart.ShoppingCartStub;
+import fr.alma.servicesshop.shoppingcart.ShoppingCartStub.Add;
 import fr.alma.servicesshop.supplier.SupplierStub;
 import fr.alma.servicesshop.supplier.SupplierStub.GetProducts;
 import fr.alma.servicesshop.supplier.SupplierStub.SupplierProductBean;
@@ -18,13 +21,27 @@ import java.util.ArrayList;
  */
 public class ServiceShop
 {
-				
-	public SupplierProductBean[] getProducts() throws AxisFault, RemoteException{
+
+	
+	public int add(int client,long idProduct,int quantity) throws AxisFault, RemoteException {
+		
+		ShoppingCartStub shoppingStub = new ShoppingCartStub();
+		
+		ShoppingCartStub.Add addingProd= new Add();
+		addingProd.setClient(client);
+		addingProd.setProductId(idProduct);
+		addingProd.setQuantity(quantity);
+		
+		ShoppingCartStub.AddResponse res = shoppingStub.add(addingProd);
+		
+		return res.get_return();
+	}
+	
+	public SupplierProductBean[] getProducts() throws AxisFault, RemoteException {
 	
 		SupplierStub stub = new SupplierStub();
 		
 		SupplierStub.GetProducts prod = new  GetProducts();
-		// prod.setParam pour passer les paramètres. 
 		
 		SupplierStub.GetProductsResponse products = stub.getProducts(prod);
 
@@ -34,16 +51,19 @@ public class ServiceShop
 	
 	
 	
-	public Boolean commandProduct(Long id) throws AxisFault, RemoteException {
-		
-		SupplierStub stub = new SupplierStub();
-		
-		SupplierStub.CommandProduct prod = new  GetProducts();
-		// prod.setParam pour passer les paramètres. 
-		
-		SupplierStub.CommandProductResponse products = stub.getProducts(prod);
-
-		return products.get_return();
-	}
+//	
+//	public Boolean commandProduct(Long id) throws AxisFault, RemoteException {
+//		
+//		ShoppingCartStub shoppingStub = new ShoppingCartStub();
+//		
+//		ShoppingCartStub.GetShopingCart addingProd= new Add();
+//		addingProd.setClient(client);
+//		addingProd.setProductId(idProduct);
+//		addingProd.setQuantity(quantity);
+//		
+//		ShoppingCartStub.AddResponse res = shoppingStub.add(addingProd);
+//		
+//		return res.get_return();
+//	}
 	
 }
