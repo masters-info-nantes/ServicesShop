956cae047add6317bd42ae2adf9c003e6cfa92c2
Dralagen <dralagen@dralagen.fr>
2015-11-26 16:49:05 +0100

    WEB : add product on shoppingCart

--- ServiceShopWebApp/src/main/webapp/javascript/callService.js
+++ ServiceShopWebApp/src/main/webapp/javascript/callService.js
@@ -8,26 +8,42 @@ $.soap({
   data:{},
   method:'getProducts',
   success:function(response) {
-    var products = response.toJSON()["#document"]["ns:getProductsResponse"]["ns:return"]
+    // Clear products
+    $("#products").html(" ");
+
+    // Populate products
+    var products = response.toJSON()["#document"]["ns:getProductsResponse"]["ns:return"];
     $.each(products, function (id, product) {
       $("#products").append('<div class="col-sm-4"> \
-      <div class="panel panel-primary"> \
+      <div class="panel panel-primary product"> \
         <div class="panel-heading">' + product.product.name + '</div> \
         <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div> \
         <div class="panel-footer"> \
-          <p>' + product.product.price + ' ' + product.product.currency + '</p> \
-          <button class="btn btn-success btn-md addition"> \
+          <p>' + product.product.price + ' ' + product.product.currency + ' \
+          <input type="number" class="form-control" name="quantity" value="1" min="0"/></p> \
+          <button class="btn btn-success btn-md addition" id="' + product.ID["_"] + '"> \
             <span class="glyphicon glyphicon-plus"></span> add to your cart \
           </button> \
         </div> \
       </div> \
     </div>');
     });
+
+    // Add Action
+    $('.addition').click(function(product) {
+      $.soap({
+        data:{
+          client:1,
+          idProduct:product.target.id,
+          quantity:$(".product #" + product.target.id).parent().find("input[name='quantity']")[0].value
+        },
+        method:'add',
+        success:function (response) {
+          console.log(response.toJSON()["#document"]["ns:addResponse"]["ns:return"]);
+        }
+      })
+    });
   }
-})
-
-
-
-$('.addition').click(function() {
-
 });
+
+
